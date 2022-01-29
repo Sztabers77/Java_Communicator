f8e0b6c2ad101b8ef73ef095a9ef39b1fcb6f4db
Sztabers <bartoszsztaberski@wp.pl>
2022-01-29 21:35:31 +0100

    template

---
+++ src/main/java/api/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package api.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+	private String content;
+	private String sender;
+	private MessageType type;
+
+	public enum MessageType {
+		CHAT, LEAVE, JOIN
+	}
+
+
+
+}
